3f99f4b9a9ee1873e567e6f7c190c06f2cd3cf4b
Yoshio Onuki <105018988+YoshioOnuki@users.noreply.github.com>
2024-05-19 13:44:46 -0500

    feat: foto de autoridades

--- app/Models/Autoridad.php
+++ app/Models/Autoridad.php
@@ -30,6 +30,21 @@ class Autoridad extends Model
         return $this->belongsTo(Facultad::class, 'id_facultad');
     }
 
+    public function getSoloPrimerosNombresAttribute()
+    {
+        //Primero separo los nombres de los grados (Dr., Ing., Mg., etc)
+        $nombres = explode('.', $this->nombre_autoridad);
+        //Luego separo los nombres
+        $nombres = explode(' ', $nombres[1]);
+        //Retorno solo los primeros nombres
+        return $nombres[0] . ' ' . $nombres[1];
+    }
+
+    public function getMostrarFotoAttribute()
+    {
+        return $this->foto_autoridad ?? 'https://ui-avatars.com/api/?name=' . $this->solo_primeros_nombres . '&size=64&&color=e8f6f8&background=17a2b8&bold=true';
+    }
+
     public $timestamps = false;
 
 }
