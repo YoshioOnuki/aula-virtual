4c5c42bcaf43a7a2b4f51a7b76b75224a1eab2ff
Yoshio Onuki <105018988+YoshioOnuki@users.noreply.github.com>
2024-11-09 21:11:01 -0500

    feat: Rectificacion del boton para guardar respuestas de foros*

--- resources/views/livewire/gestion-aula/foro/respuesta-formulario.blade.php
+++ resources/views/livewire/gestion-aula/foro/respuesta-formulario.blade.php
@@ -49,11 +49,13 @@
                                         <div class="row g-3">
 
                                             <div class="col-lg-12">
-                                                <label for="descripcion_foro_respuesta" class="form-label required @error('descripcion_foro_respuesta') text-danger @enderror">
+                                                <label for="descripcion_foro_respuesta"
+                                                    class="form-label required @error('descripcion_foro_respuesta') text-danger @enderror">
                                                     Respuesta
                                                 </label>
                                                 <div wire:ignore>
-                                                    <textarea class="form-control" wire:model.lazy="descripcion_foro_respuesta"
+                                                    <textarea class="form-control"
+                                                        wire:model.lazy="descripcion_foro_respuesta"
                                                         id="descripcion_foro_respuesta">
                                                         {{ $descripcion_foro_respuesta }}
                                                     </textarea>
@@ -68,27 +70,33 @@
 
                                     <div class="card-footer d-flex justify-content-between">
                                         <div class="ms-auto">
-                                            <button type="submit" class="btn btn-primary w-100 mt-3"
-                                                wire:loading.attr="disabled" wire:target="guardar_respuesta, descripcion_foro_respuesta">
-                                                <span wire:loading.remove wire:target="guardar_respuesta, descripcion_foro_respuesta">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                                            <button type="submit" class="btn btn-primary w-100"
+                                                wire:loading.attr="disabled"
+                                                wire:target="guardar_respuesta, descripcion_foro_respuesta">
+                                                <span wire:loading.remove
+                                                    wire:target="guardar_respuesta, descripcion_foro_respuesta">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                        viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                                         stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                                                         class="icon icon-tabler icons-tabler-outline icon-tabler-message-plus">
                                                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                                                         <path d="M8 9h8" />
                                                         <path d="M8 13h6" />
-                                                        <path d="M12.01 18.594l-4.01 2.406v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v5.5" />
+                                                        <path
+                                                            d="M12.01 18.594l-4.01 2.406v-3h-2a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v5.5" />
                                                         <path d="M16 19h6" />
                                                         <path d="M19 16v6" />
                                                     </svg>
                                                     Registrar Respuesta
                                                 </span>
                                                 <span wire:loading wire:target="descripcion_foro_respuesta">
-                                                    <div class="spinner-border spinner-border-sm me-2" role="status"></div>
+                                                    <div class="spinner-border spinner-border-sm me-2" role="status">
+                                                    </div>
                                                     Cargando Contenido
                                                 </span>
                                                 <span wire:loading wire:target="guardar_respuesta">
-                                                    <div class="spinner-border spinner-border-sm me-2" role="status"></div>
+                                                    <div class="spinner-border spinner-border-sm me-2" role="status">
+                                                    </div>
                                                     Guardando Respuesta
                                                 </span>
                                             </button>
